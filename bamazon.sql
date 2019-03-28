1213fc50b7823008a502f94b763c8134e2ab7546
Odysseus <arwaterman@gmail.com>
2019-03-27 18:46:08 -0700

    added in departments table to sql seed script

--- bamazon.sql
+++ bamazon.sql
@@ -11,16 +11,32 @@ CREATE TABLE products (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE departments (
+    department_id SERIAL NOT NULL,
+    department_name VARCHAR(30),
+    over_head_costs DOUBLE(10, 2),
+    PRIMARY KEY (department_id)
+);
+
 INSERT INTO products (product, department, price, stock)
-VALUES  ("seal plushie", "Toys R Us", 4.5, 50),
-		("SNES", "Nintendo", 100, 20),
-        ("guitar", "Guitar Center", 500.99, 5),
-        ("I-Phone Plus", "Apple", 250, 35),
+VALUES  ("seal plushie", "Toys", 4.5, 50),
+		("SNES", "Games", 100, 20),
+        ("guitar", "Music", 500.99, 5),
+        ("I-Phone Plus", "Technology", 250, 35),
         ("book", "Library", 10, 100),
-        ("Refurbished Laptop", "Acer", 625, 2),
-        ("Leprachaun", "Fantasy Island", 9999, 1),
-        ("rubber band", "Office Depot", 0.05, 2413),
-        ("Airbook", "Apple", 1000, 12),
-        ("Super Mario World", "Nintendo", 20, 9);
-        
+        ("Refurbished Laptop", "Technology", 625, 2),
+        ("Leprachaun", "Fantasy", 9999, 1),
+        ("rubber band", "Office", 0.05, 2413),
+        ("Airbook", "Technology", 1000, 12),
+        ("Super Mario World", "Games", 20, 9);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES  ("Toys", 1000),
+        ("Games", 2000),
+        ("Music", 500.50),
+        ("Technology", 1505.05),
+        ("Fantasy", 52.22),
+        ("Office", 5050.01);
+
 -- SELECT * FROM products;
+-- SELECT * FROM departments;
